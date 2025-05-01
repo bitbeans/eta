d5afe3ea35ab3bc2957f1dded5fc7771b181f085
Christian Hermann <c.hermann@bitbeans.de>
2025-05-01 17:37:25 +0200

    Update sensors_default.py

--- custom_components/eta/sensors_default.py
+++ custom_components/eta/sensors_default.py
@@ -5,7 +5,7 @@
 @factor is mandatory
 """
 from homeassistant.components.sensor import SensorStateClass, SensorDeviceClass
-from homeassistant.const import UnitOfTemperature, UnitOfPower, UnitOfMass, UnitOfEnergy, UnitOfConductivity
+from homeassistant.const import UnitOfTemperature, UnitOfPower, UnitOfMass, UnitOfEnergy, PERCENTAGE
 
 SENSORS_DEFAULT = [
     {
@@ -42,7 +42,7 @@ SENSORS_DEFAULT = [
     },
     {
         "uri": "/120/10601/0/0/12528",
-        "unit": UnitOfConductivity.PERCENTAGE,
+        "unit": PERCENTAGE,
     },
     {
         "name": "Gesamt Energieverbrauch",
